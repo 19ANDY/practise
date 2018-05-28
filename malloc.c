9ceec458afc9563c5f3bdbd473018b3796ffb2ce
Rishabh Anand <geniusrishabhanand@gmail.com>
2018-05-28 15:11:44 +0530

    Create malloc.c

---
+++ malloc.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ 
+int main() {
+    // declare a variable to store the name
+    char *name = (char *) malloc(100);
+    
+    // read the name from input
+    scanf("%s", name);
+    
+    // print the name 
+    printf("Hello %s", name);
+}
